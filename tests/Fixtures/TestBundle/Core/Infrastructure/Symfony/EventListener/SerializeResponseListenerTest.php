0cf1c0de284ec45cfc2a93a5b3ebf9f58be900f8
Marvin Courcier <courciermarvin@gmail.com>
2023-07-05 15:58:45 +0200

    :white_check_mark: (phpunit) adding phpunit and first unit test to project

---
+++ tests/Fixtures/TestBundle/Core/Infrastructure/Symfony/EventListener/SerializeResponseListenerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ApiScout project.
+ *
+ * Copyright (c) 2023 ApiScout
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiScout\Tests\Fixtures\TestBundle\Core\Infrastructure\Symfony\EventListener;
+
+use ApiScout\Core\Domain\Pagination\Factory\PaginatorRequestFactoryInterface;
+use ApiScout\Core\Domain\Resource\Factory\ResourceFactoryInterface;
+use ApiScout\Core\Infrastructure\Symfony\EventListener\SerializeResponseListener;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+use function defined;
+
+final class SerializeResponseListenerTest extends TestCase
+{
+    public function testDoNotHandleResponse(): void
+    {
+        $resourceFactory = $this->createStub(ResourceFactoryInterface::class);
+        $paginatorRequestFactory = $this->createStub(PaginatorRequestFactoryInterface::class);
+        $normalizer = $this->createStub(NormalizerInterface::class);
+
+        $listener = new SerializeResponseListener(
+            $resourceFactory,
+            $paginatorRequestFactory,
+            $normalizer
+        );
+
+        $event = new ViewEvent(
+            $this->createStub(HttpKernelInterface::class),
+            new Request(),
+            defined(HttpKernelInterface::class.'::MAIN_REQUEST') ? HttpKernelInterface::MAIN_REQUEST : HttpKernelInterface::MASTER_REQUEST,
+            null
+        );
+
+        $listener->onKernelView($event);
+
+		Assert::assertNull($event->getResponse());
+    }
+}
