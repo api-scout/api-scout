f78bbebdb681e6b55fc0d717539e93e40a23a5eb
Marvin Courcier <marvincourcier.dev@gmail.com>
2023-10-15 14:51:02 +0200

    :recycle: (config) updating testbundle configuration

---
+++ tests/Fixtures/app/config/packages/framework.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the ApiScout project.
+ *
+ * Copyright (c) 2023 ApiScout
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpFoundation\Session\SessionFactory;
+
+return static function (ContainerConfigurator $container): void {
+    $array = [
+        'secret' => 'marvin.fr',
+        'validation' => ['enable_annotations' => true],
+        'serializer' => ['enable_annotations' => true],
+        'test' => null,
+        'session' => class_exists(SessionFactory::class) ? ['storage_factory_id' => 'session.storage.factory.mock_file'] : ['storage_id' => 'session.storage.mock_file'],
+        'profiler' => [
+            'enabled' => true,
+            'collect' => false,
+        ],
+        'router' => ['utf8' => true],
+        'http_method_override' => false,
+    ];
+
+    $container->extension('framework', $array);
+};
